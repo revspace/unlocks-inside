7a5d0e76faa453d1ae487978581977f22bee8f5c
Peetz0r <peter@haas-en-berg.nl>
2022-09-24 00:20:18 +0200

    september 2022 changes
    basically more rebootproofing

--- restart.sh
+++ restart.sh
@@ -1,11 +1,6 @@
 #!/bin/bash -x
-if pgrep -f unlocks-local; then
-  echo already running, quitting
-  exit 42
-fi;
-
 cd /home/peetz0r/unlocks-local/
 date +%Y-%m-%d-%H:%M:%S | tee -a starts.log
-#screen -S unlocks-local -X kill;
+screen -S unlocks-local -X kill;
 #screen -dmS unlocks-local bash -xc 'while true; do sleep 2; ./unlocks-local 2>&1 >> $(date +%Y-%m-%d_%H:%M:%S.log); done;';
 screen -dmS unlocks-local bash -xc 'python3 unlocks-local -i';
